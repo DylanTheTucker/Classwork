06333465b3422c3f56d90c36d26312d2ce675b87
DylanTheTucker <donealtucker@gmail.com>
2025-08-21 12:58:08 -0500

    Added Code

---
+++ Dylan_Tucker_hello/src/HelloWorld.java
@@ -0,0 +1,22 @@
+public class HelloWorld { 
+    String name; 
+
+    //Class constructor
+    public HelloWorld(String name) { 
+        this.name = name; 
+    }
+
+    //function
+    public void greet() { 
+        System.out.println("Hello, " + name + "!"); 
+    } 
+
+    //Main Body
+    public static void main(String[] args) 
+    { 
+        //Construct HelloWorld Object
+        HelloWorld student = new HelloWorld("Jason");
+
+        student.greet(); 
+    } 
+}
